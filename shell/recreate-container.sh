b11d5fe8b22f14a1457a7ffcc1fe685a3455479f
Ryan Lee <gxlhybh@gmail.com>
2020-05-02 20:46:20 +0800

    Update recreate-container.sh

--- shell/recreate-container.sh
+++ shell/recreate-container.sh
@@ -2,13 +2,11 @@
 BASEDIR=$(dirname "$0")
 source $BASEDIR/color-shell.sh
 echo -e "${BGreen}Recreate docker container${Color_Off}" && \
-docker-compose up -d --no-deps --build --force-recreate && \
+docker-compose up -d --no-deps --build --force-recreate --remove-orphans && \
 docker-compose stop && \
 echo -e "${BGreen}Start container"${Color_Off} && \
-docker-compose start mysql && \
-docker-compose start redis && \
-docker-compose start cupoj-backend && \
-docker-compose start cupoj-judger && \
+docker-compose start mysql redis && \
+docker-compose start cupoj-backend cupoj-judger && \
 docker-compose start cupoj-websocket && \
 docker-compose start nginx && \
 echo -e "${BGreen}Containers have been started.${Color_Off}";
