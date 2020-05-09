cbce5129d5e0c1809ae4eabac8b99299537b0e0c
Ryan Lee <gxlhybh@gmail.com>
2020-05-10 02:03:08 +0800

    Update frontend shell

---
+++ shell/download-latest-frontend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+
+LATEST_VERSION=$(curl "https://api.github.com/repos/ryanlee2014/CUP-Online-Judge-CDN/tags" | grep '"name":' | sed -E 's/.*"([^"]+)".*/\1/' | head -n 1)
+
+mkdir /tmp/cupoj-frontend
+
+cd /tmp/cupoj-frontend
+
+wget "https://api.github.com/repos/ryanlee2014/CUP-Online-Judge-CDN/zipball/$LATEST_VERSION"
+
+unzip "$LATEST_VERSION"
+
+UNZIP_DIR_NAME=$(ls | grep ryan)
+
+cd ./$UNZIP_DIR_NAME
+
+rm -rf "$BASEDIR/wwwroot/html/*"
+
+cp -r * "$BASEDIR/wwwroot/html"
+
+cd "$BASEDIR"
+
+rm -rf /tmp/cupoj-frontend
