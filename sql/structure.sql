88c4a3996e16cd861b8a85e62054cfa4e8856778
Ryan Lee <gxlhybh@gmail.com>
2020-05-19 18:44:29 +0800

    Fix databases error

--- sql/structure.sql
+++ sql/structure.sql
@@ -43,7 +43,7 @@ CREATE TABLE `acm_member` (
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `article` (
-  `user_id` int(11) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
   `article_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `defunct` varchar(2) NOT NULL DEFAULT 'N',
