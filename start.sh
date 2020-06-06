c1b2d1385e98e7955709439c361650adc6519f14
Ryan Lee <gxlhybh@gmail.com>
2020-06-06 18:28:26 +0800

    Fix language server dockerfile dependency missing issue

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ if [[ "$#" != 0 ]]; then
     done
 else
     echo -e "${BGreen}Start containers${Color_Off}" && \
-    docker-compose start mysql redis && \
+    docker-compose start mysql redis cupoj-language-server && \
     docker-compose start cupoj-backend cupoj-judger && \
     docker-compose start cupoj-websocket && \
     docker-compose start nginx && \
